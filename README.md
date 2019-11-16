5ae0fcd774515cae7f4198a47bae41a8fdaf83f0
nateGeorge <wordsforthewise@gmail.com>
2019-11-16 10:09:12 -0700

    complete study -- write analysis; finish plots; add rest of data

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # summit_co_real_estate_analysis
-Analysis of summit county real estate sales data.
+After buying and living in a condo in the mountains for a few years, I decided it was time to sell.  I was able to qualify as living in the condo full-time due to working remotely in the summers and sometimes on long weekends during the winter, which gave me greater than 50% of my living time at the condo.  From watching the market for a few years, I had noticed prices seemed to dip at the end of the summer, and be higher in the winter ski season.  I wondered if there were any significant differences between selling in January or waiting a month of two, and analyzed past sales data to answer this question.
 
-# Data Source
-Fortunately, Summit County makes recent sales data public: http://www.co.summit.co.us/389/Sales-Reports
+## Data Source
+Fortunately, Summit County makes [recent sales data public](http://www.co.summit.co.us/389/Sales-Reports).  I just manually downloaded all the excel files since there aren't that many.
 
+## Results
+First, one reason I believed prices might be higher in the winter is due to low supply.  I had seen this in realtors' newsletters, and here is the raw data:
+
+![condo sales counts per month](images/summit_co_condo_number_of_sales.png)
+
+As we can see, the number of sales is much lower in the winter months than summer.  Obviously, lower supply is good for the seller.
+
+Looking at the price data, we can see there is no clear monthly trend in price.  There is a strong uptrend in price, which is to be expected in a "strong" economy where the value of the dollar is being depreciated significantly via QE and lower interest rates (and stocks are going up).
+
+![condo prices per SF](images/summit_co_condo_price_per_sf.png)
+
+The trend seems to be flattening out during 2019, but also flattened out in 2016 when the stock market had a weak spell.  In 2018, it looks like there was a dip in prices, which was the year I started paying close attention to prices.  This explains my belief (which I have since revised) that prices are usually lower at the end of the summer.
+
+Looking specifically at the condo complex where I own(ed) a unit, we can see the average price per square foot is almost the same as the Summit County trend:
+
+![treehouse prices per sf](images/treehouse_price_per_sf.png)
+
+Even more specifically, we can see the top-floor 1000-square-foot units (like mine) follow the trend as well.
+
+![treehouse 3nd floor prices per sf](images/3rd_floor_unit_sales_per_sf.png)
+
+Since there are so few sales of these units, this data is difficult to use to summarize trends.  The sales prices of these units depends on how well the unit is marketed and negotiated, as well as the seller's specific situation.  The condition of the units also varies widely, and can effect price significantly. For example, the $425K sale in 2019 was an extensively-remodeled unit, which was probably also expertly marketed and negotiated.  The other lower sales were not as nice of units and the sellers may have needed money quikly.
+
+![treehouse 3nd floor prices per sf](images/3rd_floor_unit_sales.png)
+
+# Conclusion
+In conclusion, it appears there is no specific advantage to selling at any particular time of year for top-floor 1000-sq-ft Treehouse condo units.  However, condo supply in Summit County is lowest around December through March, which coincides with the peak ski season.  This is not suprising, as no one wants to sell a ski condo during the season.  However, basic supply and demand economics principles dictates the best time to sell is in the peak ski season and the best time to buy is during the late part of summer or early fall.  Prices do not seem to reflect this exectation, but perhaps other metrics such as days on market and number of offers do.
+
+Further research into this topic could include modeling the data with fbprohpet to extract trends (e.g. long-term trends and monthly trends), as well as looking at other data not available from the public dataset such as days on market and the pictures and text from the MLS listing.
--- analyze_data.py
+++ analyze_data.py
@@ -1,3 +1,7 @@
+"""
+filepaths are relative; this is intended to be run from the home directory of the repo
+"""
+
 import os
 import glob
 
@@ -11,6 +15,8 @@ def load_data():
     data source: http://www.co.summit.co.us/389/Sales-Reports
     """
     files = glob.glob('data/*.xls*')
+    monthly_files = glob.glob('data/2019/*.xls*')
+    files += monthly_files
     dfs = [pd.read_excel(f) for f in files]
     all_data = pd.concat(dfs)
     all_data.reset_index(inplace=True)
@@ -36,13 +42,15 @@ def load_data():
             'SFLA',
             'Econ',
             'NHood',
-            'price per SF']
+            'price per SF',
+            'Property Desc']
 
 
     """
     Office Date -- appears to be date the deed was signed and ownership transferred
 
     Sales Valid -- sales validation code; e.g. 90, 91 are good sales
+    0 is skip -- not sure what this means
     51 is partial interest -- should exclude these for full sales
     10/11 are related parties -- ignore
     40 is court ordered (ignore)
@@ -58,8 +66,32 @@ def load_data():
     # keep only good sales codes
     codes = [0, 90, 91]
     all_data_cln = all_data_cln[all_data_cln['Sales Valid'].isin(codes)]
+    # some 'Improvement Only' with code 90...seems like remodeling or something
+    all_data_cln = all_data_cln[~all_data_cln['Property Desc'].str.contains('Improvement Only')]
+
+    # some sales with very low price per SF for some reason, and some with very high
+    # all_data_cln = all_data_cln[all_data_cln['price per SF'] > 20]
+
+    # ignore office dates before 2014
+    all_data_cln = all_data_cln[all_data_cln.index.year >= 2014]
+
+    # some SF measurements of 0; some of these are land
+    all_data_cln = all_data_cln[all_data_cln['SFLA'] > 0]
 
     return all_data_cln
+
+
+def flatten_multiindex(df):
+    """
+    flattens a multiindex dataframe where indices are month and years
+
+    returns a dataframe with a datetimeindex
+    """
+    flat_idx = df.index.to_flat_index()
+    flat_idx_dt = pd.DatetimeIndex([pd.to_datetime(str(f[1]) + '-' + str(f[0])) for f in flat_idx])
+    flat_df = pd.DataFrame(index=flat_idx, data=df.to_dict())
+    flat_df.set_index(flat_idx_dt, inplace=True)
+    return flat_df
 
 
 if __name__ == '__main__':
@@ -67,24 +99,87 @@ if __name__ == '__main__':
 
     # get treehouse data
     th_df = all_data_cln[all_data_cln['Subdiv Desc'].str.contains('TREEHOUSE')]
-    # only get 3rd floor units
+    # maybe a typo or something, but one unit with very low price
+    th_df = th_df[th_df['Sale Price'] > 10000]
+    # only get 3rd floor units with ~1000 sq ft
     th_df_3 = th_df[th_df['SFLA'] == 1008]
 
-    # not super informative
-    th_df_3['Sale Price'].iplot(asPlot=True)
+    # plot sales price of third floor units
+    filename = 'images/3rd_floor_unit_sales.png'
+    fig = th_df_3['Sale Price'].iplot(kind='scatter',
+                                        mode='lines+markers',
+                                        size=7,
+                                        opacity=0.5,
+                                        asFigure=True,
+                                        xTitle='date',
+                                        yTitle='USD sales price',
+                                        title='individual USD sales price of 3rd floor treehouse condos')
+    fig.write_image(filename, scale=3)
+
+    # plot price per SF for 3rd floor units
+    filename = 'images/3rd_floor_unit_sales_per_sf.png'
     th_df_3_med = th_df_3.groupby([th_df_3.index.year, th_df_3.index.month]).median()
-    th_df_3_med['price per SF'].iplot(asPlot=True)
+    flat_df = flatten_multiindex(th_df_3_med)
+    fig = flat_df['price per SF'].iplot(kind='scatter',
+                                        mode='lines+markers',
+                                        size=7,
+                                        opacity=0.5,
+                                        asFigure=True,
+                                        xTitle='date',
+                                        yTitle='USD sales price per SF',
+                                        title='monthly USD sales price per SF of 3rd floor treehouse condos')
+    fig.write_image(filename, scale=3)
 
     # look at all treehouse
+    # overall price histogram is skewed (possibly from time)
+    # so use median as central tendency measure
+    # th_df['price per SF'].iplot(kind='histogram', asPlot=True)
+    # sales_count = th_df.groupby([th_df.index.year, th_df.index.month]).count()
+    # sales_count.iplot(asPlot=True)
+
     th_med = th_df.groupby([th_df.index.year, th_df.index.month]).median()
-    th_med['price per SF'].iplot(asPlot=True)
+    
+    flat_df = flatten_multiindex(th_med)
+
+    filename = 'images/treehouse_price_per_sf.png'
+    fig = flat_df['price per SF'].iplot(kind='scatter',
+                                        mode='lines+markers',
+                                        size=7,
+                                        opacity=0.5,
+                                        asFigure=True,
+                                        xTitle='date',
+                                        yTitle='USD sales price per SF',
+                                        title='monthly USD sales price per SF of treehouse condos')
+    fig.write_image(filename, scale=3)
 
 
     # look at all condos
     condos = all_data_cln[all_data_cln['Subdiv Desc'].str.contains('CONDO')]
+    # some very high price per SF, must be a mistake or something
+    condos = condos[condos['price per SF'] < 1500]
     condo_med = condos.groupby([condos.index.year, condos.index.month]).median()
-    condo_med['price per SF'].iplot(asPlot=True)
+    flat_df = flatten_multiindex(condo_med)
+    filename = 'images/summit_co_condo_price_per_sf.png'
+    fig = flat_df['price per SF'].iplot(kind='scatter',
+                                        mode='lines+markers',
+                                        size=7,
+                                        opacity=0.5,
+                                        asFigure=True,
+                                        xTitle='date',
+                                        yTitle='USD sales price per SF',
+                                        title='monthly USD sales price per SF of Summit County condos')
+    fig.write_image(filename, scale=3)
 
-    # TODO: add 2019 data and plot
-    # fbprophet model and examine monthly trends
-    # look at distribution of prices in a month and decide median vs mean or otherwise
+    # plot number of sales
+    filename = 'images/summit_co_condo_number_of_sales.png'
+    condo_count = condos.groupby([condos.index.year, condos.index.month]).count()
+    flat_df = flatten_multiindex(condo_count)
+    fig = flat_df['price per SF'].iplot(kind='scatter',
+                                        mode='lines+markers',
+                                        size=7,
+                                        opacity=0.5,
+                                        asFigure=True,
+                                        xTitle='date',
+                                        yTitle='number of sales',
+                                        title='number of Summit County condo sales by month')
+    fig.write_image(filename, scale=3)
